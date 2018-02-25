3be349ec5cde8b445c1dd4a47580cf90f3a185c1
Akash D <akash.dambale1@gmail.com>
2018-02-25 13:10:03 +0530

    commit 2.1

--- HelloGit2502/src/com/samrat/HelloMain.java
+++ HelloGit2502/src/com/samrat/HelloMain.java
@@ -4,6 +4,12 @@ public class HelloMain {
 
 	public static void main(String[] args) {
 System.out.println("Hello world");
+
+int[] i = {1 , 3, 8 , 10};
+for (int j : i) {
+	System.out.println(j);
+	
+}
 	}
 
 }
